483058aecda842db8eb70fe2f72c635c3b566d35
Sergey Stukach <sergeyst80@gmail.com>
2021-02-11 22:04:49 +0300

    Dict_web_all_func

--- src/dictionaries/views.py
+++ src/dictionaries/views.py
@@ -1,44 +1,53 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from . import models
-# from dictionaries.models import Authors, Countries, Genres, Publishers, Series
-
-models_dict = {
-    'Authors': models.Authors,
-    'Countries': models.Countries.objects.all(),
-    'Genres': models.Genres.objects.all(),
-    'Publishers': models.Publishers.objects.all(),
-    'Series': models.Series.objects.all()
-}
-
-# authors = models.Authors
-# countries = models.Countries.objects.all()
-# genres = models.Genres.objects.all()
-# publishers = models.Publishers.objects.all()
-# series = models.Series.objects.all()
+from django.urls import reverse
 
 
 # Create your views here.
-# def main_page(request):
-#     return render(request, template_name='main.html', context={'models_dict': models_dict})
-
 
 def dict_view(request):
     context = {'object': models.Authors.objects.all()}
     return render(request, template_name='dict_view.html', context=context)
 
+
 def dict_detail(request, pk):
     context = {'object': models.Authors.objects.get(pk=pk)}
     return render(request, template_name='dict_detail.html', context=context)
 
+
 def dict_update(request, pk):
     context = {'object': models.Authors.objects.get(pk=pk)}
     return render(request, template_name='dict_detail.html', context=context)
+
 
 def dict_delete(request, pk):
     context = {'object': models.Authors.objects.get(pk=pk)}
     models.Authors.objects.get(pk=pk).delete()
     return render(request, template_name='dict_delete.html', context=context)
 
+
 def dict_add(request):
-    context = {'object': models.Authors.objects.all()} 
-    return render(request, template_name='dict_view.html', context=context)
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        date_of_birth = request.POST.get('date_of_birth')
+        models.Authors.objects.create(first_name=first_name, last_name=last_name, date_of_birth=date_of_birth)
+        return HttpResponseRedirect(reverse('dict-view'))
+    
+    context = {} 
+    return render(request, template_name='dict_add.html', context=context)
+
+
+def dict_update(request, pk):
+    author = models.Authors.objects.get(pk=pk)
+    print(author.first_name, author.last_name, author.date_of_birth)
+    if request.method == "POST":
+        author.first_name = request.POST.get('first_name')
+        author.last_name = request.POST.get('last_name')
+        author.date_of_birth = request.POST.get('date_of_birth')
+        author.save()
+        return HttpResponseRedirect(reverse('dict-view'))
+    
+    context = {'author': author} 
+    return render(request, template_name='dict_update.html', context=context)
--- src/proj/urls.py
+++ src/proj/urls.py
@@ -24,8 +24,6 @@ urlpatterns = [
     path('dict-detail/<int:pk>/', views.dict_detail, name='dict-detail'),
     path('dict-update/<int:pk>/', views.dict_update, name='dict-update'),
     path('dict-delete/<int:pk>/', views.dict_delete, name='dict-delete'),
-    path('dict-add/', views.dict_add, name='dict-add')
-    # path('dict_view/<str:dict_name>/', views.dict_view, name='dict-view')
-    # path('', dicts_page)
-    # path('', dicts_page)
+    path('dict-add/', views.dict_add, name='dict-add'),
+    path('dict-update/<int:pk>/', views.dict_update, name='dict-update')
 ]
