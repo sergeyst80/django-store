fb032f1a89ebc5d11c76c3193915c0f20ae171b1
Sergey Stukach <sergeyst80@gmail.com>
2021-02-26 09:43:42 +0300

    Dicts_main_03

--- src/mainapp/admin.py
+++ src/mainapp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from mainapp import models as mainapp_models
+
+
+class MainAppAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk', 
+        'name',
+        'picture',
+        'cost',
+        'get_authors',
+        'series',
+        'get_genres',
+        'year',
+        'num_pages',
+        'book_format',
+        'isbn',
+        'weight',
+        'age_category',
+        'publisher',
+        'qty',
+        'is_active',
+        'rating',
+        'create_date',
+        'update_date'
+    ]
+
+admin.site.register(mainapp_models.BookCard, MainAppAdmin)
