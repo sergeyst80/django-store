2b912c7b70f93cacb9de560955827bfd2af046ab
Sergey Stukach <sergeyst80@gmail.com>
2021-03-02 22:34:38 +0300

    Ref_Main_Bootstrap_03

--- src/mainapp/views.py
+++ src/mainapp/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from mainapp import models as mainapp_models
+from mainapp import models
 
 
 # Create your views here.
 class HomePage(generic.TemplateView):
-    model = mainapp_models.BookCard
+    model = models.BookCard
     template_name = "mainapp/homepage.html"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        books = mainapp_models.BookCard.objects.all().order_by('-pk')[0:9]
+        books = self.model.objects.all().order_by('-pk')[0:9]
         context['objects'] = books
         return context
